23596b3437cceb9befbdf91751d02be0ad4fccf2
Joseph Isbell <shadowwolf347@comcast.net>
2018-07-12 08:56:17 -0600

    Added comments and punctuation

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 		<link rel="stylesheet" href="./css/styles.css" type="text/css" />
 	</head>
 	<body>
+<!--		General Information-->
 		<div>
 			<h1>About Toby Parker</h1>
 			<img src="./toby.jpg" alt="Picture of Toby Parker" />
@@ -17,29 +18,31 @@
 			<p><span class="bold">Profession:</span>College Student</p>
 		</div>
 		<div>
-			<h3>Attitude</h3>
+<!--			Attitude of Persona-->
+			<h3>Attitude:</h3>
 			<p>Toby is a college student who loves music and attending concerts. His motto is "Study hard, party hard".
 				He especially loves being able to share his experiences with his friends. He is the type to live in the moment
 				and wants to be able to post reviews on the go.</p>
-
-			<h3>Needs</h3>
+<!--				Needs and Frustrations-->
+			<h3>Needs:</h3>
 			<ul>
-				<li>Add reviews about venues he attended for concerts</li>
-				<li>Post and upload pictures of the venue and concerts</li>
-				<li>Share his favorites with friends</li>
+				<li>Add reviews about venues he attended for concerts.</li>
+				<li>Post and upload pictures of the venue and concerts.</li>
+				<li>Share his favorites with friends.</li>
 			</ul>
-			<h3>Frustrations</h3>
+			<h3>Frustrations:</h3>
 			<ul>
-				<li>Does not want to go through multiple menus</li>
-				<li>Wants to be able to add comments to other's reviews</li>
+				<li>Does not want to go through multiple menus.</li>
+				<li>Wants to be able to add comments to other's reviews.</li>
 			</ul>
-			<h3>Technology</h3>
+<!--			Tech owned and comfort level-->
+			<h3>Technology:</h3>
 			<ul>
 				<li>Comfort Level: High</li>
-				<li>Wants to get the newest version phone possible but sometimes can't afford to</li>
-				<li>Owns a Google Pixel 1 XL, Android 7.1 OS, 5.5in screen</li>
-				<li>Dell G3579-5941BLK-PUS G3 15 3579 - 15.6" LED Display - 8th Gen Intel i5 Processor Windows 10 OS</li>
-				<li>Mainly uses phone for browsing the web and uses laptop for school and gaming</li>
+				<li>Wants to get the newest version phone possible but sometimes can't afford to.</li>
+				<li>Owns a Google Pixel 1 XL, Android 7.1 OS, 5.5in screen.</li>
+				<li>Dell G3579-5941BLK-PUS G3 15 3579 - 15.6" LED Display - 8th Gen Intel i5 Processor Windows 10 OS.</li>
+				<li>Mainly uses phone for browsing the web and uses laptop for school and gaming.</li>
 			</ul>
 		</div>
 	</body>
