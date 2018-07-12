ce9fe08276d654b18b68d7c4747a30450c8e5fed
Joseph Isbell <shadowwolf347@comcast.net>
2018-07-12 08:30:39 -0600

    Added index.php file and added needs and information of persona

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<title>Snap Concert Persona</title>
+		<link rel="stylesheet" href="./css/styles.css" type="text/css" />
+	</head>
+	<body>
+		<div>
+			<h1>About Toby Parker</h1>
+			<img src="./toby.jpg" alt="Picture of Toby Parker" />
+		</div>
+		<div>
+			<p><span class="bold">Name:</span>Toby Parker</p>
+			<p><span class="bold">Gender:</span>Male</p>
+			<p><span class="bold">Age:</span>21</p>
+			<p><span class="bold">Profession:</span>College Student</p>
+		</div>
+		<div>
+			<h3>Attitude</h3>
+			<p>Toby is a college student who loves music and attending concerts. His motto is "Study hard, party hard". He especially loves being able to share his experiences with his friends. He is the type to live in the moment and wants to be able to post reviews on the go</p>
+
+			<h3>Needs</h3>
+			<ul>
+				<li>Add reviews about venues he attended for concerts</li>
+				<li>Post and upload pictures of the venue and concerts</li>
+				<li>Share his favorites with friends</li>
+			</ul>
+			<h3>Frustrations</h3>
+			<ul>
+				<li>Does not want to go through multiple menus</li>
+				<li>Wants to be able to add comments to other's reviews</li>
+			</ul>
+		</div>
+	</body>
+</html>
