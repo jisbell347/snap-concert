aba563237b9b2808eb5efe2bff770925c6c5d368
Joseph Isbell <shadowwolf347@comcast.net>
2018-07-12 12:21:31 -0600

    Added content from index to persona.php

---
+++ use-case.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<title>Snap Concert Persona</title>
+		<link rel="stylesheet" href="./css/styles.css" type="text/css" />
+	</head>
+	<body>
+		<div>
+			<h1>Use Case</h1>
+			<h3>User Story</h3>
+			<p>Toby wants to be able to post and share reviews of concert venues</p>
+		</div>
+		<div>
+			<h3>Interaction Flow</h3>
+			<ul>
+				<li>Toby clicks on input field to type in a review</li>
+				<li>Site captures his information in the input field and displays his text</li>
+				<li>Toby clicks on the post review button</li>
+				<li>Site displays his review</li>
+				<li>Toby clicks to share his review with his friend</li>
+				<li>Site informs his friend that Toby shared the review</li>
+			</ul>
+		</div>
+	</body>
+</html>
